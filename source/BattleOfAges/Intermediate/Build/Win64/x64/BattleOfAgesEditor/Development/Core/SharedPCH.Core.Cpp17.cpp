e489a3d976b6f6a5d35bc3b4f45cad425c577d0a
Adrian Martinez <adrianjustinmartinez@gmail.com>
2023-12-09 11:50:40 -0800

    init commit

---
+++ source/BattleOfAges/Intermediate/Build/Win64/x64/BattleOfAgesEditor/Development/Core/SharedPCH.Core.Cpp17.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.38.33133
+#include "D:/git/battle-of-ages/source/BattleOfAges/Intermediate/Build/Win64/x64/BattleOfAgesEditor/Development/Core/SharedPCH.Core.Cpp17.h"
---
+++ source/BattleOfAges/Intermediate/Build/Win64/x64/BattleOfAgesEditor/Development/Engine/SharedPCH.Engine.Cpp17.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Engine/Public/EngineSharedPCH.h
+#include "D:/git/battle-of-ages/source/BattleOfAges/Intermediate/Build/Win64/x64/BattleOfAgesEditor/Development/Engine/SharedDefinitions.Engine.Cpp17.h"
+#include "Runtime/Engine/Public/EngineSharedPCH.h"
